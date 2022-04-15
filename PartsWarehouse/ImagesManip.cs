85c16c659b1652784c0adc15d2dc916229ebae8e
Imagara <milick33@gmail.com>
2022-04-16 01:21:34 +0700

    /

---
+++ PartsWarehouse/ImagesManip.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace PartsWarehouse
+{
+    internal class ImagesManip
+    {
+        public static byte[] BitmapSourceToByteArray(BitmapSource image)
+        {
+            #region Кодирование картинки
+            using (var stream = new MemoryStream())
+            {
+                var encoder = new PngBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(image));
+                encoder.Save(stream);
+                return stream.ToArray();
+            }
+            #endregion
+        }
+
+        public static BitmapImage SelectImage()
+        {
+            #region Выбор картинки
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Выбрать изображение";
+            op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+                        "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+                        "Portable Network Graphic (*.png)|*.png";
+            if (op.ShowDialog() == true)
+                return new BitmapImage(new Uri(op.FileName));
+            else
+                return null;
+            #endregion
+        }
+
+        //public static BitmapImage NewImage(Chat chat)
+        //{
+        //    MemoryStream ms = new MemoryStream(chat.ImgSource);
+        //    BitmapImage image = new BitmapImage();
+        //    image.BeginInit();
+        //    image.StreamSource = ms;
+        //    image.EndInit();
+        //    return image;
+        //}
+
+        public static BitmapImage NewImage(User user)
+        {
+            MemoryStream ms = new MemoryStream(user.ProfileImgSource);
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            image.StreamSource = ms;
+            image.EndInit();
+            return image;
+        }
+    }
+}
--- PartsWarehouse/MainWindow.xaml.cs
+++ PartsWarehouse/MainWindow.xaml.cs
@@ -15,60 +15,36 @@ using System.Windows.Shapes;
 
 namespace PartsWarehouse
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            MainFrame.Content = new СatalogPage();
         }
 
-        private void SendMessage(string nickName, string message, string date, BitmapImage imageSource)
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            Grid messageGrid = new Grid();
-            messageGrid.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0x40, 0x44, 0x4B));
-            messageGrid.Height = 45;
-            messageGrid.Width = 590;
-            messageGrid.Margin = new Thickness(10, 5, 10, 5);
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
 
-            Image messageImage = new Image();
-            messageImage.Source = imageSource;
-            messageImage.Width = 35;
-            messageImage.Height = 35;
-            messageImage.Margin = new Thickness(5);
-            messageImage.HorizontalAlignment = HorizontalAlignment.Left;
-            messageGrid.Children.Add(messageImage);
+        private void ButtonMininize_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
 
-            StackPanel stackpanel = new StackPanel();
-            stackpanel.Orientation = Orientation.Horizontal;
+        private void WindowStateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.MainWindow.WindowState != WindowState.Maximized)
+                Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            else
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+        }
 
-            Label authorLabel = new Label();
-            authorLabel.Content = nickName;
-            authorLabel.Foreground = Brushes.White;
-            authorLabel.FontWeight = FontWeights.Bold;
-            authorLabel.HorizontalAlignment = HorizontalAlignment.Left;
-            authorLabel.VerticalAlignment = VerticalAlignment.Top;
-            authorLabel.Margin = new Thickness(40, 0, 0, 0);
-
-            Label dateLabel = new Label();
-            dateLabel.Content = date;
-            dateLabel.Foreground = Brushes.White;
-
-            stackpanel.Children.Add(authorLabel);
-            stackpanel.Children.Add(dateLabel);
-            messageGrid.Children.Add(stackpanel);
-
-            Label messageLabel = new Label();
-            messageLabel.Content = message;
-            messageLabel.Foreground = Brushes.White;
-            messageLabel.HorizontalAlignment = HorizontalAlignment.Left;
-            messageLabel.VerticalAlignment = VerticalAlignment.Bottom;
-            messageLabel.Margin = new Thickness(40, 0, 0, 0);
-            messageGrid.Children.Add(messageLabel);
-
-            PartsListBox.Items.Add(messageGrid);
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
