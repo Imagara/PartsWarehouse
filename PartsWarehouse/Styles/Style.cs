75ef19c1b8fbd6a1426c4f5a8f14ce6c5147e883
Imagara <milick33@gmail.com>
2022-04-27 03:09:07 +0700

    test

--- PartsWarehouse/Styles/Style.cs
+++ PartsWarehouse/Styles/Style.cs
@@ -1,17 +1,18 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Input;
 
 namespace PartsWarehouse
 {
     public partial class Style : ResourceDictionary
     {
-        private void MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        private void MouseEnter(object sender, MouseEventArgs e)
         {
             ((Border)sender).Background = new SolidColorBrush(Color.FromArgb(0xFF, 0x2F, 0x31, 0x36));
         }
 
-        private void MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        private void MouseLeave(object sender, MouseEventArgs e)
         {
             ((Border)sender).Background = new SolidColorBrush(Color.FromArgb(0xFF, 0x33, 0x36, 0x3C));
         }
